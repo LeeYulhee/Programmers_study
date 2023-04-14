03356bd2081ef75d939b3df8f004b9e76e7b34de
LeeYulhee <125900734+LeeYulhee@users.noreply.github.com>
2023-04-14 14:56:54 +0900

    [level 0] Title: 평행, Time: 0.05 ms, Memory: 68.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120875. 평행/평행.java
+++ 프로그래머스/lv0/120875. 평행/평행.java
@@ -2,10 +2,7 @@ class Solution {
     public int solution(int[][] dots) {
         int answer = 0;
         
-        if(dots[0][0] - dots[1][0] == 0) {
-            if(slope(dots[0][0], dots[2][0], dots[0][1], dots[2][1]) == slope(dots[1][0], dots[3][0], dots[1][1], dots[3][1])) answer = 1;
-        }
-        else if (slope(dots[0][0], dots[1][0], dots[0][1], dots[1][1]) == slope(dots[2][0], dots[3][0], dots[2][1], dots[3][1])) answer = 1;
+        if (slope(dots[0][0], dots[1][0], dots[0][1], dots[1][1]) == slope(dots[2][0], dots[3][0], dots[2][1], dots[3][1])) answer = 1;
         
         return answer;
     }
