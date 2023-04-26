ad9aab056225f6769624c5beec8f2689f47f17e7
LeeYulhee <125900734+LeeYulhee@users.noreply.github.com>
2023-04-26 13:46:02 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 1.97 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -1,25 +1,14 @@
 class Solution {
-    public String solution(String s) {
-        String[] str = s.split(" ", -1);
+  public String solution(String s) {
 
-        StringBuilder sb = new StringBuilder();
+        String answer = "";
+        int cnt = 0;
+        String[] array = s.split("");
 
-
-        for(int index = 0; index < str.length; index++) {
-            String temp = str[index];
-            for(int i = 0; i < temp.length(); i++) {
-                if(i % 2 == 0 && (temp.charAt(i) >= 'a' && temp.charAt(i) <= 'z')) {
-                    sb.append(String.valueOf((char) (temp.charAt(i) - 32)));
-                } else if (i % 2 != 0 && (temp.charAt(i) >= 'A' && temp.charAt(i) <= 'Z')) {
-                    sb.append(String.valueOf((char) (temp.charAt(i) + 32)));
-                } else {
-                    sb.append(String.valueOf(temp.charAt(i)));
-                }
-            }
-            
-            if(index == str.length - 1) break;
-            sb.append(" ");
+        for(String ss : array) {
+            cnt = ss.contains(" ") ? 0 : cnt + 1;
+            answer += cnt%2 == 0 ? ss.toLowerCase() : ss.toUpperCase(); 
         }
-        return sb.toString();
-    }
+      return answer;
+  }
 }
