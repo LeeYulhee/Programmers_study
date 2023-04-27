7755996cf48bde039fff32b132126ca3e8413904
LeeYulhee <125900734+LeeYulhee@users.noreply.github.com>
2023-04-27 21:23:47 +0900

    [level 2] Title: 기능개발, Time: 0.12 ms, Memory: 77.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Solution {
+    public List solution(int[] progresses, int[] speeds) {
+        List<Integer> list = new ArrayList<>();
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        for (int i = 0; i < progresses.length; i++) {
+           queue.add((100 - progresses[i] + (speeds[i]) - 1) / speeds[i]);
+        }
+
+        int day = queue.poll();
+        int count = 1;
+        int i = 0;
+
+        while(!queue.isEmpty()) {
+            if(day >= queue.peek()) {
+                count++;
+                queue.poll();
+            } else if (day < queue.peek()) {
+                list.add(count);
+                day = queue.poll();
+                count = 1;
+            }
+            if (queue.peek() == null) {
+                list.add(count);
+            }
+        }
+        return list;
+    }
+}
