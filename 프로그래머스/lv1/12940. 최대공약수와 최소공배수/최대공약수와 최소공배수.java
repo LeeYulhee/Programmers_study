cb8a9e7aba116ef743040602c3aefc08c03f6662
LeeYulhee <125900734+LeeYulhee@users.noreply.github.com>
2023-05-10 09:37:08 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.02 ms, Memory: 82.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] solution(int n, int m) {
+        int[] answer = {gcd(n, m), lcm(n, m)};
+        return answer;
+    }
+    public static int gcd(int n, int m) {
+        if (m == 0) {
+            return n;
+        }
+        return gcd(m, n % m);
+    }
+
+    public static int lcm(int n, int m) {
+        return n * m / gcd(n, m);
+    }
+}
