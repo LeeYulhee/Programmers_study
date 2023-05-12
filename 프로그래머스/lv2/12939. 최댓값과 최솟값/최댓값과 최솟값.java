965c552db2dfc0c5985fa1f28ca02d34b6c8e4c6
LeeYulhee <125900734+LeeYulhee@users.noreply.github.com>
2023-05-12 15:37:44 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 1.32 ms, Memory: 75.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String s) {
+        String[] str = s.split(" ");
+        int[] arr = new int[str.length];
+
+        for(int i = 0; i < str.length; i++) {
+            arr[i] = Integer.parseInt(str[i]);
+        }
+
+        Arrays.sort(arr);
+        return String.format("%d %d", arr[0], arr[arr.length - 1]);
+    }
+}
