859ac6343d21ba54784e06099d0cfa2f28879831
LeeYulhee <125900734+LeeYulhee@users.noreply.github.com>
2023-07-20 14:54:12 +0900

    [level 1] Title: 소수 찾기, Time: 16.12 ms, Memory: 53.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int solution(int n) {
+        
+        int answer = 0;
+
+        boolean[] isPrime = new boolean[n + 1];
+
+        for(int i = 2; i <= n; i++) {
+            isPrime[i] = true;
+        }
+
+        for(int i = 2; i * i <= n; i++) {
+            if(isPrime[i]) {
+                for(long j = i * i; j <= n; j += i) {
+                    isPrime[(int)j] = false;
+                }
+            }
+        }
+
+        for(boolean b : isPrime) {
+            if(b) answer++;
+        }
+        return answer;
+    }
+}
