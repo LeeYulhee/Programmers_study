eb0899f12502e2513cd9a9579388397c831398ef
LeeYulhee <125900734+LeeYulhee@users.noreply.github.com>
2023-09-08 01:31:36 +0900

    [level 2] Title: 숫자의 표현, Time: 0.02 ms, Memory: 51.7 MB -BaekjoonHub

--- 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.java
@@ -2,11 +2,13 @@ class Solution {
     public int solution(int n) {
         int answer = 0;
         
-        for(int i = 0; i < n; i++) {
-            n = n - i;
-            if(n % (i + 1) == 0) {
+        for(int i = 1; i <= n; i++) {
+            
+            if(n % i == 0) {
                 answer++;
             }
+            
+            n = n - i;
         }
         
         return answer;
