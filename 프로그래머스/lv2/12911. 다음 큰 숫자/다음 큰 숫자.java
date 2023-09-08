3cfebe9e3e7d33fa0c542efdc0c49f84ffac8040
LeeYulhee <125900734+LeeYulhee@users.noreply.github.com>
2023-09-09 02:31:28 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.06 ms, Memory: 52.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n) {
+        
+        int oneCounter = Integer.toBinaryString(n).replace("0", "").length();
+        int findNum = n + 1;
+                
+        while(true) {
+            int check = Integer.toBinaryString(findNum).replace("0", "").length();
+            if(check == oneCounter) return findNum;
+            findNum++;
+        }
+    }
+}
