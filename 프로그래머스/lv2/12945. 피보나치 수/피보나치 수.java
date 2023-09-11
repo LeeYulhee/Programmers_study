ba175ea919840d560a590a5e78417c2ef587971c
LeeYulhee <125900734+LeeYulhee@users.noreply.github.com>
2023-09-11 21:44:04 +0900

    [level 2] Title: 피보나치 수, Time: 1.57 ms, Memory: 77.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution { 
+    public int solution(int n) {        
+        int answer = 1;
+        int answerBefore = 0;
+        
+        for(int i = 2; i <= n; i++) {
+            int temp = answer;
+            answer = (answer + answerBefore) % 1234567;
+            answerBefore = temp;
+        }
+        
+        return (int)answer;
+        
+    }
+}
