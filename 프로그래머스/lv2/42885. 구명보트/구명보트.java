a176af2a068d59d0010991704c901560ba08c321
LeeYulhee <125900734+LeeYulhee@users.noreply.github.com>
2023-09-18 22:55:11 +0900

    [level 2] Title: 구명보트, Time: 10.05 ms, Memory: 53.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42885. 구명보트/구명보트.java
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.java
@@ -2,34 +2,12 @@ import java.util.Arrays;
 
 class Solution {
     public int solution(int[] people, int limit) {
-        
-        int right = people.length - 1;
-        int answer = 0;
-        
         Arrays.sort(people);
-        
-        for(int i = 0; i < people.length; i++) {
-            int left = i;
-            
-            if(right < left) break;
-            
-            while(left < right) {
-                
-                if(people[left] + people[right] <= limit) {
-                    answer++;
-                    people[left] = 0;
-                    people[right] = 0;
-                    right--;
-                    break;
-                }
-                right--;
-            }
+        int i = 0, j = people.length - 1;
+        for (; i < j; --j) {
+            if (people[i] + people[j] <= limit)
+                ++i;
         }
-        
-        for(int i : people) {
-            if (i != 0) answer++;
-        }
-        
-        return answer;
+        return people.length - i;
     }
 }
