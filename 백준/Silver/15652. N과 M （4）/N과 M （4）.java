9de0ee30984d33ed738a2260f75b1cf9ed85c9e6
LeeYulhee <125900734+LeeYulhee@users.noreply.github.com>
2025-01-16 13:24:38 +0900

    [Silver III] Title: N과 M (4), Time: 392 ms, Memory: 28848 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,35 @@
+import java.util.Stack;
+import java.util.Scanner;
+
+class Main {
+
+    static int loopIndex;
+    static int step;
+    static Stack<Integer> stack = new Stack<>();
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        loopIndex = sc.nextInt();
+        step = sc.nextInt();
+
+        recursive(0);
+    }
+
+    private static void recursive(int index) {
+
+        if (index == step) {
+            System.out.println(stack.toString().replace(",", "").replace("[", "").replace("]", ""));
+            return;
+        }
+        
+        for (int i = 1; i < loopIndex + 1; i++) {
+            if (!stack.isEmpty() && stack.peek() >= i + 1) continue;
+            
+            stack.push(i);
+            recursive(index + 1);
+            stack.pop();
+        }
+    }
+}
