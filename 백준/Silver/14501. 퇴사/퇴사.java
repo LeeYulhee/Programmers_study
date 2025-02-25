5609122cff8497a54c38a64437f3a4759c1aa846
LeeYulhee <125900734+LeeYulhee@users.noreply.github.com>
2025-02-25 16:12:57 +0900

    [Silver III] Title: 퇴사, Time: 172 ms, Memory: 17780 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -2,38 +2,41 @@ import java.util.Scanner;
 
 public class Main {
 
-    static int day;
-    static int salary = Integer.MIN_VALUE;
+    static int     day;
     static int[][] schedule;
+    static int[]   dp;
+    static int     salary;
 
     public static void main(String[] args) {
-
+        
         Scanner sc = new Scanner(System.in);
 
         day = sc.nextInt();
-        schedule = new  int[day][2];
+        schedule = new int[day][2];
+        dp = new int[day + 1];
 
-        for(int i = 0; i < day; i++) {
+        for (int i = 0; i < day; i++) {
             schedule[i][0] = sc.nextInt();
             schedule[i][1] = sc.nextInt();
         }
 
-        recursive(0, 0);
-
-        System.out.println(salary);
-    }
-
-    static void recursive(int index, int fee) {
-
-        if ( index <= day ) {
-            salary = Math.max(salary, fee);
+        for (int i = 0; i <= day; i++) {
+            dp[i] = -1;
         }
 
-        if ( index >= day) return;
+        System.out.println(recursive(0));
+    }
 
-        recursive(index + schedule[index][0], fee + schedule[index][1]);
-        recursive(index + 1, fee);
+    private static int recursive (int index) {
 
+        if ( index == day ) return 0;
 
+        if ( index > day - 1 ) return Integer.MIN_VALUE;
+
+        if ( dp[index] != -1 ) return dp[index];
+
+        dp[index] = Math.max( recursive(index + schedule[index][0]) + schedule[index][1], recursive(index + 1) );
+
+        return dp[index];
     }
 }
