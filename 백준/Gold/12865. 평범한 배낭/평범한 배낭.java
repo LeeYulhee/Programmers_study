e73082a77b57a77bce991f8dce204d261c27c72f
LeeYulhee <125900734+LeeYulhee@users.noreply.github.com>
2025-02-28 03:26:02 +0900

    [Gold V] Title: 평범한 배낭, Time: 256 ms, Memory: 56984 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,44 +1,29 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
-
-    static int     count;
-    static int     maxWeight;
-    static int[][] things;
-    static int[][]   dp;
-    static int     salary;
-
     public static void main(String[] args) {
-        
         Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+        int[] weight = new int[N + 1];
+        int[] value = new int[N + 1];
+        int[][] dp = new int[N + 1][K + 1];
 
-        count = sc.nextInt();
-        maxWeight = sc.nextInt();
-        things = new int[count][2];
-        dp = new int[count + 1][100001];
-
-        for (int i = 0; i < count; i++) {
-            things[i][0] = sc.nextInt();
-            things[i][1] = sc.nextInt();
+        for (int i = 1; i <= N; i++) {
+            weight[i] = sc.nextInt();
+            value[i] = sc.nextInt();
         }
 
-        for (int i = 0; i < dp.length; i++) {
-            Arrays.fill(dp[i], -1);
+        for (int i = 1; i <= N; i++) {
+            for (int w = 0; w <= K; w++) {
+                if (weight[i] > w) {  
+                    dp[i][w] = dp[i - 1][w];
+                } else {  
+                    dp[i][w] = Math.max(dp[i - 1][w], dp[i - 1][w - weight[i]] + value[i]);
+                }
+            }
         }
 
-        System.out.println(recursive(0, 0));
-    }
-
-    private static int recursive (int index, int weight) {
-
-        if ( weight > maxWeight ) return Integer.MIN_VALUE;
-
-        if ( index == count ) return 0;
-
-        if ( dp[index][weight] != -1 ) return dp[index][weight];
-
-        dp[index][weight] = Math.max( recursive(index + 1, weight + things[index][0]) + things[index][1], recursive(index + 1, weight) );
-
-        return dp[index][weight];
+        System.out.println(dp[N][K]);
     }
 }
