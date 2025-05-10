80d146b601f4e90151567bcdd9b3482a424ede3d
LeeYulhee <125900734+LeeYulhee@users.noreply.github.com>
2025-05-10 16:11:12 +0900

    [Silver III] Title: 바이러스, Time: 104 ms, Memory: 14296 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -4,45 +4,48 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
+
         int computer = Integer.parseInt(br.readLine());
-        int connect = Integer.parseInt(br.readLine());
-        
-        Map<Integer, List<Integer>> map = new HashMap<>();
-        
-        for(int i = 0; i < connect; i++) {
-            String[] strArray = br.readLine().split(" ");
-            
-            map.computeIfAbsent(Integer.parseInt(strArray[0]), k -> new ArrayList<>()).add(Integer.parseInt(strArray[1]));
-            map.computeIfAbsent(Integer.parseInt(strArray[1]), k -> new ArrayList<>()).add(Integer.parseInt(strArray[0]));
+        int connection = Integer.parseInt(br.readLine());
+
+        List<List<Integer>> graph = new ArrayList<>();
+        for (int i = 0; i <= computer; i++) {
+            graph.add(new ArrayList<>());
         }
-        
-        Queue<Integer> queue = new LinkedList<>();
-        boolean[] check = new boolean[computer + 1];
-        int count = 0;
-        
-        queue.add(1);
-        check[1] = true;
-        
-        while(!queue.isEmpty()) {
-            int num = queue.poll();
-            
-            if(map.get(num) != null) {
-                for(int i : map.get(num)) {
-                    if(check[i] == false) {
-                        queue.add(i);
-                        check[i] = true;
-                        count++;
-                    }
+
+        for (int i = 0; i < connection; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            graph.get(a).add(b);
+            graph.get(b).add(a);
+        }
+
+        boolean[] visited = new boolean[computer + 1];
+        Deque<Integer> deque = new LinkedList<>();
+        deque.addLast(1); // 또는 deque.offerLast(1);
+
+        while (!deque.isEmpty()) {
+            int current = deque.pollFirst();
+            visited[current] = true;
+
+            for (int neighbor : graph.get(current)) {
+                if (!visited[neighbor]) {
+                    deque.addLast(neighbor);
                 }
             }
-            
-            check[num] = true;
         }
-        
-        System.out.println(count);
+
+        int count = 0;
+        for (int i = 1; i <= computer; i++) {
+            if (visited[i]) count++;
+        }
+
+        System.out.println(count - 1);
     }
 }
