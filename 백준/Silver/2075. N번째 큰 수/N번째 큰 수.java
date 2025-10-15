865e1606a8ad37b579fcd12cc8f1912538a6bde0
LeeYulhee <125900734+LeeYulhee@users.noreply.github.com>
2025-10-16 00:41:24 +0900

    [Silver III] Title: N번째 큰 수, Time: 740 ms, Memory: 212336 KB -BaekjoonHub

--- 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
@@ -7,29 +7,20 @@ import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int lines = Integer.parseInt(br.readLine());
-        Queue<Integer> queue = new PriorityQueue<>((a, b) -> a - b);
+        Queue<Integer> queue = new PriorityQueue<>(lines);
 
-        // 메모리 초과 발생 => 힙 정렬을 최대 2,250,000개나 해서 그런듯..
-        // 무조건 윗줄의 값이 아랫줄보다 작다고 했다는 걸 고려해야 할 것 같음
-        // 내 생각엔 큐의 사이즈를 항상 N개로 정해놓고 제거 + 넣기를 반복해야 될 것 같기도 하고..
         for (int i = 0; i < lines; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
 
             for (int j = 0; j < lines; j++) {
                 int current = Integer.parseInt(st.nextToken());
 
-                // 큐가 비어있지 않고, queue의 크기가 lines와 같으면
-                if (!queue.isEmpty() && queue.size() >= lines) {
-                    int queueNumber = queue.peek();
-                    if (queueNumber < current) {
-                        queue.poll();
-                        queue.add(current);
-                    }
-                } else {
+                if (queue.size() < lines) queue.add(current);
+                else if (current > queue.peek()) {
+                    queue.poll();
                     queue.add(current);
                 }
             }
