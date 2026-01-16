e935ef8d986f3c5eb2645b17801812dbeb9567da
LeeYulhee <125900734+LeeYulhee@users.noreply.github.com>
2026-01-16 22:54:46 +0900

    [Silver II] Title: 값이 k인 트리 노드의 깊이, Time: 492 ms, Memory: 65216 KB -BaekjoonHub

--- 백준/Silver/25511. 값이 k인 트리 노드의 깊이/값이 k인 트리 노드의 깊이.java
+++ 백준/Silver/25511. 값이 k인 트리 노드의 깊이/값이 k인 트리 노드의 깊이.java
@@ -5,10 +5,6 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        // 부모 자식이 주어짐
-        // 해당 노드의 'value'가 따로 주어짐
-        // k == value 일 때, depth를 출력
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
@@ -16,15 +12,12 @@ public class Main {
         int K = Integer.parseInt(st.nextToken());
 
         List<Integer>[] connect = new ArrayList[N];
-        int[] parents = new int[N];
         int[] values = new int[N];
         int[] nodeDepth = new int[N];
 
         for (int i = 0; i < N; i++) {
             connect[i] = new ArrayList<>();
         }
-
-        Arrays.fill(parents, -1);
 
         for (int i = 0; i < N - 1; i++) {
             st = new StringTokenizer(br.readLine());
@@ -33,7 +26,6 @@ public class Main {
             int nextNode = Integer.parseInt(st.nextToken());
 
             connect[node].add(nextNode);
-            parents[nextNode] = node;
         }
 
         st = new StringTokenizer(br.readLine());
@@ -42,18 +34,11 @@ public class Main {
             values[i] = Integer.parseInt(st.nextToken());
         }
 
-        int root = 0;
-
-        for (int i = 0; i < N; i++) {
-            if (parents[i] == -1) root = i;
-        }
-
         Queue<Integer> queue = new LinkedList<>();
-        queue.add(root);
+        queue.add(0);
 
         while (!queue.isEmpty()) {
             int current = queue.poll();
-//            int parent = parents[current];
             int depth = nodeDepth[current];
 
             for (int i = 0; i < connect[current].size(); i++) {
